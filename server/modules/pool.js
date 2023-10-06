40d767ba32663e1303246362a3afad592689ad58
jcolago <jcolago2005@gmail.com>
2023-10-06 16:38:02 -0500

    Added history and onClick function to MovieList.jsx

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -21,7 +21,7 @@ else {
     pool = new pg.Pool({
         host: 'localhost',
         port: 5432,
-        database: 'saga_movie_weekend', 
+        database: 'saga_movies_weekend', 
     });
 }
 
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function Details() {
+    return(
+    <h1>Details</h1>
+    )
+}
