30255fb2e0dc00c9125a4bb655935fcf53149187
jcolago <jcolago2005@gmail.com>
2023-10-08 17:49:59 -0500

    Added documentation to App.jsx and Header.jsx files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
+//Imports from react and router
 import { HashRouter as Router, Route } from 'react-router-dom';
+//Css file import
 import './App.css';
+//Component imports
 import Header from '../Header/Header';
 import MovieList from '../MovieList/MovieList'
 import Details from '../Details/Details';
 
 
 function App() {
-
+  // Renders the app
   return (
     <div className="App">
       <div className='header'>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
+//Import for react
 import React from "react";
 
-
+//Renders the header component
 export default function Header() {
     return (
         <h1>The Movies Saga!</h1>
